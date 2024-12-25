7ad36293b193ba26896776d724c881776d8ca649
Alen Raj <alenraj246@gmail.com>
2024-12-26 00:15:34 +0530

    view engine set up

---
+++ controllers/user/userController.js
@@ -0,0 +1,17 @@
+
+const loadHomepage=async(req,res)=>{
+    try {
+        return res.render('user/home')
+    } catch (error) {
+        console.log('Home page not found');
+        res.status(500).send('server error')
+    }
+}
+
+
+
+
+
+module.exports={
+    loadHomepage
+}
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 const express=require('express')
 const app=express()
+const path=require('path')
 const env=require('dotenv').config()
 const DB=require('./Database/db')
+const userRouter=require("./routes/userRouter")
 DB()
 
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+
+
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'views'))
+app.use(express.static('public'))
+
+
+app.use('/user',userRouter)
+
+
 const port=process.env.PORT
-
-
 app.listen(port,()=>{
     console.log(`server run on port ${port}`)
 })
